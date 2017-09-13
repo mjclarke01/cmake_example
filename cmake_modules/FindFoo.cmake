fd9bbd6d2ce8fc8b2c4d76950390972ae0d835f1
mjc23 <matt.clarke@stfc.ac.uk>
2017-09-13 09:25:24 +0100

    Don't need namespace

--- cmake_modules/FindFoo.cmake
+++ cmake_modules/FindFoo.cmake
@@ -1,14 +1,14 @@
 find_path(GTEST_INCLUDE_DIR gtest/gtest.h)
 find_library(GTEST_LIB NAMES gtest)
 find_library(GTEST_LIB_MAIN NAMES gtest_main)
-mark_as_advanced(GTEST_INCLUDE_DIR GTEST_LIB_MAIN GTEST_LIB)
 
+# This checks that we have found everything listed
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GTEST
         REQUIRED_VARS GTEST_LIB_MAIN GTEST_LIB GTEST_INCLUDE_DIR
         )
 
-if(GTEST_FOUND AND NOT TARGET GTEST::GTEST)
+if(GTEST_FOUND AND NOT TARGET GTEST)
     add_library(gtest UNKNOWN IMPORTED)
     set_target_properties(gtest PROPERTIES
             IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
