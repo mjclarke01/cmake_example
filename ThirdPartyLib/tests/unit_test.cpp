e30065def46636d1ba37d791ee56bd0696f25f6a
Matt Clarke <mjclarke01@yahoo.co.uk>
2017-09-16 13:50:31 +0100

    Tests work standalone

--- ThirdPartyLib/tests/unit_test.cpp
+++ ThirdPartyLib/tests/unit_test.cpp
@@ -8,3 +8,8 @@ namespace {
   }
 
 }
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <functional>
-
-int main() {
-  [out = std::ref(std::cout << "Hello, ")](){ out.get() << "World!" << std::endl; }();
-  return 0;
-}
