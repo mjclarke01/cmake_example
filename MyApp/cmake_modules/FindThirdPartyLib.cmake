24c0a8800c0716cb1141fc329978fb98a8187b0b
mjclarke01 <mjclarke01@yahoo.co.uk>
2017-09-18 11:47:21 +0100

    Update FindThirdPartyLib.cmake

--- MyApp/cmake_modules/FindThirdPartyLib.cmake
+++ MyApp/cmake_modules/FindThirdPartyLib.cmake
@@ -1,3 +1,7 @@
+# Use this type of Find module for 3rd party libraries that do not support CMake clients.
+# i.e. they don't provide for using CMake to include them. For example: Boost doesn't but QT does.
+cmake_minimum_required(VERSION 3.5)
+
 find_path(ThirdPartyLib_INCLUDE_DIR ThirdPartyLib/third_party_lib.h)
 find_library(ThirdPartyLib_LIB NAMES ThirdPartyLib)
 
@@ -8,6 +12,7 @@ find_package_handle_standard_args(ThirdPartyLib
         )
 
 if(ThirdPartyLib_FOUND AND NOT TARGET ThirdPartyLib::ThirdPartyLib)
+    # UNKNOWN means we don't know if it is static or shared
     add_library(ThirdPartyLib::ThirdPartyLib UNKNOWN IMPORTED)
     set_target_properties(ThirdPartyLib::ThirdPartyLib PROPERTIES
             IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
