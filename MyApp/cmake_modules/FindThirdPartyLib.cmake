73883ae44907abedf8e804bc9b6b5f28362ddb63
Matt Clarke <matt.clarke@stfc.ac.uk>
2017-10-26 13:24:03 +0100

    Dropped cmake version to match travis

--- MyApp/cmake_modules/FindThirdPartyLib.cmake
+++ MyApp/cmake_modules/FindThirdPartyLib.cmake
@@ -1,6 +1,6 @@
 # Use this type of Find module for 3rd party libraries that do not support CMake clients.
 # i.e. they don't provide for using CMake to include them. For example: Boost doesn't but QT does.
-cmake_minimum_required(VERSION 3.5)
+cmake_minimum_required(VERSION 3.2)
 
 find_path(ThirdPartyLib_INCLUDE_DIR ThirdPartyLib/third_party_lib.h)
 find_library(ThirdPartyLib_LIBRARY NAMES ThirdPartyLib)
